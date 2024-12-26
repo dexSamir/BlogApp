17908e93f8bef270dc16552c8a17274c70e1d995
dexSamir <samirah-bp215@code.edu.az>
2024-12-26 19:10:41 +0400

    create layers and create baseEntity then create category entity

---
+++ BloggApp/BlogApp.Core/Entities/Common/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApp.Core.Entities.Common; 
+
+public class BaseEntity
+{
+    public int Id { get; set; }
+    public bool IsDeleted { get; set; } 
+    public DateTime CreatedTime { get; set; } 
+    public DateTime? UpdatedTime { get; set; }   
+
+
+}
